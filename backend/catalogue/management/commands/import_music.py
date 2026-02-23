6f88f95abfc42f77eabf815189eddc20b7411ec4
ebenebarde <eliasschaefer05@gmail.com>
2026-02-24 00:06:30 +0100

    Commit for project

--- backend/catalogue/management/commands/import_music.py
+++ backend/catalogue/management/commands/import_music.py
@@ -40,7 +40,15 @@ class Command(BaseCommand):
             return
 
         headers = {
-            'Authorization': f'Bearer {token}'
+            'Authorization': f'Bearer {token}',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin'
         }
 
         album_ids = [
