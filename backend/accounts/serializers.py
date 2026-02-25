835ef7651950cd51905b9c309d74ef546732a351
ebenebarde <eliasschaefer05@gmail.com>
2026-02-25 15:32:52 +0100

    Fix email assignment in user creation

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -55,7 +55,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         """
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],  # Email is mandatory in custom model
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
